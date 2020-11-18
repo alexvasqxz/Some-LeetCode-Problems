b657a14ab922e2f070a3a3fcd19fa19b78568af8
Gustavo Vasquez <gustavovasquez99@icloud.com>
2020-11-17 17:01:44 -0700

    Update and rename maxIslandArea.cpp to [Medium] maxIslandArea.cpp

--- [Medium] maxIslandArea.cpp
+++ [Medium] maxIslandArea.cpp
@@ -22,16 +22,14 @@ public:
     
     int maxArea = 0;
     int temp = 0;
-    stack<pair<int,int>> visited;
     
     void getArea(vector<vector<int>>& grid, int r, int c){
-        pair<int,int> p = {r,c};
-        if (visited.find(p) != visited.end() || r < 0 || c < 0 || r >= grid.size() || c >= grid[0].size() || grid[r][c] == 0){
+        if (r < 0 || c < 0 || r >= grid.size() || c >= grid[0].size() || grid[r][c] == 0){
             return;
         }
         
-        visited.insert(p);
         temp++;
+        grid[r][c] = 0;
         getArea(grid,r+1,c);
         getArea(grid,r-1,c);
         getArea(grid,r,c+1);
