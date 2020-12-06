f7fca092c7bb800cd4b0da7dd31e19421d880749
Gustavo Vasquez <gustavovasquez99@icloud.com>
2020-12-05 19:27:22 -0700

    Max Depth

---
+++ maxDepthBinaryTree.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <math.h>
+#include <list>
+#include <set>
+#include <string>
+#include <sstream>
+#include <iterator>
+#include <stack>
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm>
+#include <numeric>
+using namespace std;
+
+struct TreeNode{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(): val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x): val(x), left(nullptr), right(nullptr){}
+    TreeNode(int x, TreeNode *left, TreeNode *right): val(x), left(left), right(right) {}
+};
+
+class Solution{
+public:
+    
+    int maxDepth(TreeNode* root){
+        if(!root){
+            return 0;
+        }
+        int left = maxDepth(root->left);
+        int right = maxDepth(root->right);
+        
+        return left > right ? left+1 : right+1;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
