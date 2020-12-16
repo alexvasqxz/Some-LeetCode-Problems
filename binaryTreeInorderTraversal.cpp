d3079dcda995187b7cabc94a2d04d5602ac8151a
Gustavo Vasquez <gustavovasquez99@icloud.com>
2020-12-16 16:44:36 -0700

    Inorder Traversal

---
+++ binaryTreeInorderTraversal.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <math.h>
+#include <list>
+#include <set>
+#include <string>
+#include <sstream>
+#include <iterator>
+#include <stack>
+#include <unordered_set>
+#include <unordered_map>
+#include <algorithm>
+#include <numeric>
+using namespace std;
+
+struct TreeNode{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(): val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x): val(x), left(nullptr), right(nullptr){}
+    TreeNode(int x, TreeNode *left, TreeNode *right): val(x), left(left), right(right) {}
+};
+
+class Solution{
+public:
+    
+    vector<int> inorder;
+    vector<int> inorderTraversal(TreeNode* root){
+        if (!root){
+            return inorder;
+        }
+        
+        inorderTraversal(root->left);
+        inorder.push_back(root->val);
+        inorderTraversal(root->right);
+        
+        return inorder;
+    }
+};
+
+int main(){
+    
+    return 0;
+}
+
