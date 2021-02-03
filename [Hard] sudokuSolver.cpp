6bc2f4e75337a2d5056db4ab4f5e13ef27495326
Gustavo Vasquez <gustavovasquez99@icloud.com>
2021-02-02 23:14:32 -0700

    [Backtracking] Sudoku Solver

--- [Hard] sudokuSolver.cpp
+++ [Hard] sudokuSolver.cpp
@@ -1,3 +1,18 @@
+//
+//  Topic: Backtracking
+//      Problem: Sudoku Solver
+//
+//  Created by Gustavo Vasquez
+//  Copyright © 2021 Alex Vasquez. All rights reserved.
+
+/*
+ A sudoku solution must satisfy all of the following rules:
+
+ Each of the digits 1-9 must occur exactly once in each row.
+ Each of the digits 1-9 must occur exactly once in each column.
+ Each of the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
+ The '.' character indicates empty cells.
+ */
 
 #include <stdio.h>
 #include <iostream>
